cb207e441936c3f05ac89d0ec9a4658c5ffdeb9a
Bar <Barmagnezi22@gmail.com>
2015-06-24 22:45:47 +0300

    Post added

---
+++ Project/mvc4_poject/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvc4_poject.Models
+{
+    public class Comment
+    {
+        public long id { get; set; }
+        public long postId { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string body { get; set; }
+
+        public class CommentDBContext : DbContext
+        {
+            public DbSet<Comment> Comments { get; set; }
+        }
+    }
+}
---
+++ Project/mvc4_poject/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvc4_poject.Models
+{
+    public class Post
+    {
+        public long id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string IdAuthor { get; set; }
+        public string date { get; set; }
+
+        public string intro { get; set; }
+        public string body { get; set; }
+
+        public string category { get; set; }
+        public string images { get; set; }
+        public string video { get; set; }
+    }
+    public class PostDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
